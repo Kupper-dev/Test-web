450e85d9f117438893b6b52d00364833009a04a0
kuppermx <admin@kupper.mx>
2026-06-04 20:02:19 -0600

    chore: install matter-js and configure vite overwhelming page entry

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
         devlink2: resolve(__dirname, 'devlink-2.html'),
         lusion: resolve(__dirname, 'lusion.html'),
         ticketsSandbox: resolve(__dirname, 'tickets-sandbox/index.html'),
+        overwhelming: resolve(__dirname, 'overwhelming.html'),
       },
     },
   },
