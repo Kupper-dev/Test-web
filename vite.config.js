a3d806fcd0b248835211ccbba99dfd97b616e6e6
kuppermx <admin@kupper.mx>
2026-06-04 23:17:30 -0600

    build: remove devlink-2 entry point from vite config

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,6 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         about: resolve(__dirname, 'about.html'),
         devlink: resolve(__dirname, 'devlink.html'),
-        devlink2: resolve(__dirname, 'devlink-2.html'),
         lusion: resolve(__dirname, 'lusion.html'),
         ticketsSandbox: resolve(__dirname, 'tickets-sandbox/index.html'),
         overwhelming: resolve(__dirname, 'overwhelming.html'),
