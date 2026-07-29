b99df2d6ecf72758679a3c83624df93f1f78cf99
kuppermx <admin@kupper.mx>
2026-07-28 22:21:43 -0600

    feat: replace 2D SVG paths with WebGL glass tube container

--- src/devlink/ItFlowSection.jsx
+++ src/devlink/ItFlowSection.jsx
@@ -65,28 +65,8 @@ export function ItFlowSection() {
       <h2>¿Qué áreas conecta IT Support Flow?</h2>
       <div className="it-flow-cards">
 
-        {/* SVG Signal paths background */}
-        <svg className="it-network-paths w-embed" viewBox="0 0 740 2000" preserveAspectRatio="xMidYMid slice" aria-hidden="true">
-          <defs>
-            <path id="itSignalPath1" d="m 106,45h 375c 114,0 226,128 226,235v 236c 0,136 -122,222 -224,221l -182,-2c -89,1 -141,42 -142,158l -2,204c -1,117 37,173 134,173h 186c 110,-3 230,111 230,220v 242c 0,113 -125,225 -248,225H 105" />
-            <path id="itSignalPath2" d="m 33,85h 444c 96,0 190,107 190,201v 224c 0,116 -98,188 -190,187l -192,-2c -92,0 -166,75 -166,168v 278c 0,94 74,169 166,169h 194c 92,0 188,94 188,188v 228c 0,94 -104,191 -214,191H 105" />
-            <path id="itSignalPath3" d="m 155,127h 308c 94,0 162,86 162,177v 178c 0,109 -50,174 -166,173L 277,653C 158,653 77,762 77,849v 302c 0,118 107,196 180,197l 204,4c 92,0 164,67 164,160v 200c 0,91 -89,163 -188,163H 105" />
-            <linearGradient id="itSignalGradient" gradientUnits="objectBoundingBox" x1="0" y1="0" x2="0.2" y2="1">
-              <stop offset="0%" stopColor="#C8C2B5" stopOpacity="0" />
-              <stop offset="15%" stopColor="#C8C2B5" stopOpacity="0.7" />
-              <stop offset="35%" stopColor="#D4CFC4" stopOpacity="1" />
-              <stop offset="50%" stopColor="#E0DBD0" stopOpacity="0.5" />
-              <stop offset="65%" stopColor="#C8C2B5" stopOpacity="0.9" />
-              <stop offset="85%" stopColor="#D4CFC4" stopOpacity="0.6" />
-              <stop offset="100%" stopColor="#C8C2B5" stopOpacity="0" />
-            </linearGradient>
-          </defs>
-          <use href="#itSignalPath1" />
-          <use href="#itSignalPath2" />
-          <use href="#itSignalPath3" />
-        </svg>
-
         {/* 1. Ventas / Mesa de Ayuda IT */}
+
         <div className="it-card-wrapper">
           <div className="it-card">
             <h3 className="it-card__title">Gestión de Helpdesk IT</h3>
